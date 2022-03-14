b5ed677ce2a2220fecb615117c5838b7446e7545
noah <noah.alerm.7e4@itb.cat>
2022-03-14 19:11:04 +0100

    Configuration Fix

--- src/main/java/com/example/apirestv1/security/WebSecurityConfiguration.java
+++ src/main/java/com/example/apirestv1/security/WebSecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,27 +27,37 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 //    }
 
     //METHODS
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .inMemoryAuthentication()
+//                .passwordEncoder(encoder)
+//                .withUser("Noah")
+//                .password(encoder.encode("secret"))
+//                .roles("ADMIN");
+//    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailsService).passwordEncoder(encoder);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
-                .and()
-                .httpBasic()
-                .authenticationEntryPoint(myEntryPoint)
-                .and()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/me/**").hasRole("ADMIN") //FORBIDDEN TEST
-                .antMatchers(HttpMethod.GET, "/users/**", "/series/**").hasRole("USER")
-                .antMatchers(HttpMethod.POST, "/users/**", "/series/**").hasRole("USER")
-                .antMatchers(HttpMethod.PUT, "/series/**").hasRole("USER")
-                .antMatchers(HttpMethod.DELETE, "/series/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST, "/series/**").hasAnyRole("USER", "ADMIN")
-                .anyRequest().authenticated();
-                //.and()
-                //.csrf().disable();
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.cors()
+//                .and()
+//                .httpBasic()
+//                .authenticationEntryPoint(myEntryPoint)
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers(HttpMethod.GET, "/me/**").hasRole("ADMIN") //FORBIDDEN TEST
+//                .antMatchers(HttpMethod.GET, "/users/**", "/series/**").hasRole("USER")
+//                .antMatchers(HttpMethod.POST, "/users/**", "/series/**").hasRole("USER")
+//                .antMatchers(HttpMethod.PUT, "/series/**").hasRole("USER")
+//                .antMatchers(HttpMethod.DELETE, "/series/**").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.POST, "/series/**").hasAnyRole("USER", "ADMIN")
+//                .anyRequest().authenticated();
+//                //.and()
+//                //.csrf().disable();
+//    }
 }
